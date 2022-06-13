6324911d0df3ff4e8f07e689ae5bc705e6f7b30c
Maxim Yaroshenko <yaroshenkomaximv@gmail.com>
2022-06-13 23:12:31 +0300

    Removed ukrainian words

--- src/main/java/ua/price/desktop/objects/ModelReviewThanksPopUp.java
+++ src/main/java/ua/price/desktop/objects/ModelReviewThanksPopUp.java
@@ -3,18 +3,15 @@ package ua.price.desktop.objects;
 import org.openqa.selenium.By;
 
 public class ModelReviewThanksPopUp extends ObjectBase {
-	//public String thanksTextString = "Спасибо за отзыв!";
-	public String thanksTextString = "Дякуємо за відгук!";
+	public String thanksTextString = "";
 	
 	public By thanksText = By.xpath("//div[@class='success-submit active']/div[1]/div[1]");
 	
-	//public String waitReviewTextString = "Ваш отзыв в ближайшее время появится на сайте.";
-	public String waitReviewTextString = "Ваш відгук найближчим часом з'явиться на сайті.";
+	public String waitReviewTextString = "";
 	
 	public By waitReviewText = By.xpath("//div[@class='success-submit active']/div[1]/div[2]");
 	
-	//public String registerTextString = "Зарегистрируйтесь, чтобы следить за реакциями на Ваши отзывы:";
-	public String registerTextString = "Увійдіть, щоб стежити за реакціями на Ваші відгуки:";
+	public String registerTextString = "";
 	
 	public By registerText = By.xpath("//div[@class='success-submit active']/div[2]/div[1]/div");
 }
